7382da311995d390109a9cb18315d0440eeb9133
Thomas Breitburd <tmb76@cam.ac.uk>
2024-03-11 23:07:32 +0000

    Added src directory files for part (v) and a module file.
    Made a part_v file for part v, which will use functions from the module file (funcs)

---
+++ src/part_v.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import zeus
+import corner
+
+
+# Define posterior, likleihood, and prior for part v
+
+
+# Get the data
+
+# Slice sample from the posterior
+
+# Normalise with the evidence, through numerical integration
+
+# Plot the 2D posterior histogram
+
+# Plot the marginal posterior distributions
+
+# Get values for the parameters
+
+
