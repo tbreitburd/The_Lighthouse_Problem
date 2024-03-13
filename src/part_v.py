e9e86e09cc58795e8bbe4fffc12e8ddc1670bc28
Thomas Breitburd <tmb76@cam.ac.uk>
2024-03-13 18:24:16 +0000

    Wrote out code lines for sampling from the posterior using different algorithms
    Wrote most of the code for Metropolis Hastings and the zeus sampler
    Started writing for the emcee sampler.
    
    Still need code for the measurements of both parameters quoted in the form mean ± standard deviation, and suitable convergence diagnostic information for your chosen sampling algorithms

--- src/part_v.py
+++ src/part_v.py
@@ -1,23 +1,118 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
 import zeus
-import corner
+import os
+from . import funcs
+
+print("---------------------------------")
+print("----------- Part V --------------")
+print("---------------------------------")
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Get the data from the txt file
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+
+path = "lighthouse_flash_data.txt"
+
+# Get the parent directory of the current script
+src_dir = os.getcwd()
+proj_dir = os.path.dirname(src_dir)
+data_path = os.path.join(proj_dir, path)
+
+# Read the data
+data = np.loadtxt(data_path)
+flash_locs = data[:, 0]
+
+print("---------------------------------")
+print("---------------------------------")
+print("(a) The Data")
+print("The observed flashes have locations:")
+print(flash_locs)
+
+# Plot the data as a diagram of the lighthouse and the flashes
+# for a hypothetical lighthouse at position alpha = 0 and beta = 0.1
+funcs.plot_lighthouse(flash_locs, (0, 0.1))
+
+# Visualise the Cauchy distribution for the lighthouse problem,
+# again for hypothetical alpha = 0 and beta = 0.1
+x = np.linspace(-10, 10, 1000)
+funcs.plot_lighthouse_cauchy(0, 0.1, x)
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+# Sample from the posterior
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+print("---------------------------------")
+print("---------------------------------")
+print("(b) Sampling from the posterior")
+
+# ---------------------------------
+# Using Metropolis-Hastings
+# ---------------------------------
+print("------ Metropolis-Hastings ------")
+print(
+    "Using the Metropolis-Hastings algorithm,"
+    + " with a multivariate normal proposal distribution:"
+)
 
 
-# Define posterior, likleihood, and prior for part v
+nsteps, ndim = 100000, 2  # Define the number of steps and the number of dimensions
+init = np.ones(ndim)  # Starting point of the chain
+cov = np.array([[1.5, 0.0], [0.0, 1.5]])
 
+# Run the Metropolis-Hastings algorithm
+chain_MH, accept_frac_MH = funcs.Metroplis_Hastings(
+    nsteps, ndim, funcs.log_posterior_v, cov, init
+)
 
-# Get the data
+print("The acceptance fraction is for the Metropolis Hastings algorithm is:")
+print(accept_frac_MH)
 
-# Slice sample from the posterior
+# Get the autocorrelation time, tau, for the chain
+taus_MH = zeus.AutoCorrTime(chain_MH)
+print("Autocorrelation:", taus_MH)
 
-# Normalise with the evidence, through numerical integration
+tau_MH = max(taus_MH)
+print("tau_MH = ", tau_MH)
 
-# Plot the 2D posterior histogram
+# Plot the distribution of the samples in a corner plot
+funcs.plot_corner(chain_MH, "MH")
 
-# Plot the marginal posterior distributions
+# ---------------------------------
+# Using the zeus sampler (ensemble sampler)
+# ---------------------------------
 
-# Get values for the parameters
+print("------ Zeus Slice Sampling ------")
+print("Using the zeus sampler, with slice sampling:")
 
+nsteps, nwalkers, ndim = 10000, 10, 2
+start = np.abs(np.random.randn(nwalkers, ndim))
 
+sampler_SS = zeus.EnsembleSampler(nwalkers, ndim, funcs.log_posterior_v)
+
+sampler_SS.run_mcmc(start, nsteps)
+chain_SS = sampler_SS.get_chain(flat=True)
+
+# Acceptance fraction
+accept_frac_SS = np.mean(sampler_SS.acceptance_fraction)
+print("The acceptance fraction is for the zeus sampler is:")
+print(accept_frac_SS)
+
+# Get the autocorrelation time, tau, for the chain
+taus_SS = zeus.AutoCorrTime(sampler_SS.get_chain())
+print("Autocorrelation:", taus_SS)
+
+tau_SS = max(taus_SS)
+print("tau_SS = ", tau_SS)
+
+# Plot the distribution of the samples in a corner plot
+funcs.plot_corner(chain_SS, "SS")
+
+# ---------------------------------
+# Using the emcee sampler (emcee)
+# ---------------------------------
+
+print("------ Emcee ------")
+print("Using the emcee sampler:")
