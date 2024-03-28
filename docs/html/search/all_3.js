bb6b3cfe727c153699e3b904b2b2988d2046d83b
Thomas Breitburd <tmb76@cam.ac.uk>
2024-03-28 23:43:45 +0000

    regenerated docs

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -5,6 +5,5 @@ var searchData=
   ['chain_5fmh_2',['chain_mh',['../namespacesrc_1_1part__v.html#a15d7fe7ac0db57b34b4da30eb6d3d088',1,'src.part_v.chain_MH'],['../namespacesrc_1_1part__vii.html#a18a07476fe1229024c9a3cf65a2091cf',1,'src.part_vii.chain_MH']]],
   ['chain_5fss_3',['chain_ss',['../namespacesrc_1_1part__v.html#a54072f243b9e52f721dc1f1bb004c50e',1,'src.part_v.chain_SS'],['../namespacesrc_1_1part__vii.html#a06d0054d1371c7aedc886e6e30f2f2cd',1,'src.part_vii.chain_SS']]],
   ['chains_5fmh_4',['chains_mh',['../namespacesrc_1_1part__v.html#a5e39aa30971e9c6c8cb97b15482d9d1f',1,'src.part_v.chains_MH'],['../namespacesrc_1_1part__vii.html#ade1b3bbba36a9c8f688cbb10a3e5dd25',1,'src.part_vii.chains_MH']]],
-  ['clean_5fchain_5',['clean_chain',['../namespacesrc_1_1funcs.html#ab4b0d3d488e05bb8bd626d02310e3cf5',1,'src::funcs']]],
-  ['cov_6',['cov',['../namespacesrc_1_1part__v.html#a8e1e4d77b3e07ea4b91882ffdb39b52f',1,'src.part_v.cov'],['../namespacesrc_1_1part__vii.html#af6ed29587ce07d5117e14c9282f205be',1,'src.part_vii.cov']]]
+  ['cov_5',['cov',['../namespacesrc_1_1part__v.html#a8e1e4d77b3e07ea4b91882ffdb39b52f',1,'src.part_v.cov'],['../namespacesrc_1_1part__vii.html#af6ed29587ce07d5117e14c9282f205be',1,'src.part_vii.cov']]]
 ];
--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['cauchy_5fclt_0',['cauchy_clt',['../namespacesrc_1_1funcs.html#add25bb2025539dded73444631f94262f',1,'src::funcs']]],
-  ['clean_5fchain_1',['clean_chain',['../namespacesrc_1_1funcs.html#ab4b0d3d488e05bb8bd626d02310e3cf5',1,'src::funcs']]]
+  ['cauchy_5fclt_0',['cauchy_clt',['../namespacesrc_1_1funcs.html#add25bb2025539dded73444631f94262f',1,'src::funcs']]]
 ];
